9c8d684d3ced9caddee617887c47c4a8886ff3bd
Rizwan886 <rizwanminhas7860@gmail.com>
2023-05-02 09:57:48 +0500

    step 3 main

---
+++ app/Events/NewTrade.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
+
+class NewTrade implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $message;
+    public $time;
+    public function __construct($message)
+    {
+        $this->message=$message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        // return new PrivateChannel('channel-name');
+        return new Channel('channel-trade');
+    }
+
+    public function broadcastWith(){
+        $this->time=Carbon::now()->tz('Asia/Karachi')->format('Y-md-d H:i:s');
+
+        return[
+            'message'=>$this->message,
+            'time'=>$this->time
+        ];
+
+
+    }
+}
